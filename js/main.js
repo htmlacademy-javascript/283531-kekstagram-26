d1d23a0a6e2a7402e5552c5e5343a3a33033ecc4
Anna Ayvazyan <anayvazyan95@gmail.com>
2022-06-16 20:20:15 +0400

    correcting homework2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
-let randomInteger = function (min, max) {
+const randomInteger = function (min, max) {
   if (min >= 0 && max >= 0 && min <= 10 && max <= 10) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   return ('range does not match, please try numbers from 0 to 10 inclusive');
 
-}
-randomInteger(0, 10)
+};
+randomInteger(0, 10);
 
 // Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна. Пример использования функции:
 
-let maxStringLength = function (testString, maxLength) {
+const maxStringLength = function (testString, maxLength) {
   let stringLength = 0;
   for (let i = 0; i <= testString.length; i++) {
-    stringLength++
+    stringLength++;
   }
   if (stringLength <= maxLength) {
-   return true;
+    return true;
   } else {
     return false;
   }
-}
-maxStringLength('Hello world', 140)
+};
+maxStringLength('Hello world', 140);
